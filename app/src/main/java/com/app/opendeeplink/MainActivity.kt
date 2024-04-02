3cbf8d98900a04fb10de231b111b00e7ab8b7114
Loïc Dumas <loic.dumas02@gmail.com>
2024-04-02 17:33:26 +0200

    Add deeplink history list as composable.

--- app/src/main/java/com/app/opendeeplink/MainActivity.kt
+++ app/src/main/java/com/app/opendeeplink/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.app.opendeeplink
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.app.opendeeplink.ui.comsable.DeeplinkHistory
 import com.app.opendeeplink.ui.comsable.DeeplinkInputField
 import com.app.opendeeplink.ui.theme.OpenDeeplinkTheme
 
@@ -20,6 +22,18 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Column {
                         DeeplinkInputField()
+
+                        DeeplinkHistory(deeplinkList = listOf("tel://", "hey"),
+                            copyLink = {
+                                Toast.makeText(this@MainActivity, "Copy link $it", Toast.LENGTH_SHORT).show()
+                            },
+                            deleteLink = {
+                                Toast.makeText(this@MainActivity, "Delete link $it", Toast.LENGTH_SHORT).show()
+                            },
+                            openLink = {
+                                Toast.makeText(this@MainActivity, "Open link $it", Toast.LENGTH_SHORT).show()
+                            }
+                        )
                     }
                 }
             }
---
+++ app/src/main/java/com/app/opendeeplink/ui/comsable/DeeplinkHistoryComposable.kt
@@ -0,0 +1,134 @@
+package com.app.opendeeplink.ui.comsable
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.app.opendeeplink.R
+
+/**
+ * Display the list of deep links.
+ */
+@Composable
+fun DeeplinkHistory(
+    deeplinkList: List<String>,
+    copyLink: (deeplink: String) -> Unit,
+    deleteLink: (deeplink: String) -> Unit,
+    openLink: (deeplink: String) -> Unit,
+) {
+
+    Column {
+        Text(
+            modifier = Modifier
+                .padding(8.dp),
+            text = stringResource(id = R.string.history)
+        )
+
+        LazyColumn {
+            items(deeplinkList) { deeplink ->
+                DeeplinkItem(
+                    deeplink = deeplink,
+                    copyLink = copyLink,
+                    deleteLink = deleteLink,
+                    openLink = openLink,
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun DeeplinkItem(
+    deeplink: String,
+    copyLink: (deeplink: String) -> Unit,
+    deleteLink: (deeplink: String) -> Unit,
+    openLink: (deeplink: String) -> Unit,
+) {
+    Card(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .height(32.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = deeplink,
+            )
+
+            IconButton(onClick = { copyLink(deeplink) }) {
+                Icon(
+                    imageVector = Icons.Default.Share,
+                    contentDescription = stringResource(R.string.copy),
+                )
+            }
+
+            IconButton(onClick = { deleteLink(deeplink) }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = stringResource(R.string.delete)
+                )
+            }
+
+            IconButton(onClick = { openLink(deeplink) }) {
+                Icon(
+                    imageVector = Icons.Default.PlayArrow,
+                    contentDescription = stringResource(R.string.open_deeplink)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DeeplinkHistoryPreview() {
+    DeeplinkHistory(
+        deeplinkList = listOf(
+            "https://google.com",
+            "play://media/urn",
+            "tel://"
+        ),
+        copyLink = {},
+        deleteLink = {},
+        openLink = {}
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DeeplinkItemPreview() {
+    DeeplinkItem(
+        deeplink = "https://google.com",
+        copyLink = {},
+        deleteLink = {},
+        openLink = {}
+    )
+}
